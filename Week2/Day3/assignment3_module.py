a8037d2e7c458f7937659f7870048a12286a6dc3
AliyahSchouten <134453526+AliyahSchouten@users.noreply.github.com>
2023-07-19 16:50:33 -0400

    Add files via upload

---
+++ Week2/Day3/assignment3_module.py
@@ -0,0 +1,9 @@
+#  Has a function to calculate the square footage of a house
+#  Has a function to calculate the circumference of a circle
+#  Has a function to change feet to inches
+def square_foot(width:float,length:float):
+    return width*length
+def circumference(radius:float):
+    return(2*3.14*radius)
+def ft_to_in(feet:float):
+    return feet*12
